bb4e29f82f3ef8b3482d3bc3e52a21498229c5b8
Hastimazadeh <81362792+Hastimazadeh@users.noreply.github.com>
2022-03-17 13:20:19 +0100

    changes during the meeting

--- js/temp.js
+++ js/temp.js
@@ -2,15 +2,12 @@ function Temp (props){
 
   const [count, setCount] = React.useState(0);
 
-  
-
   React.useEffect(() => {
     setInterval(() => {
       //setCount(prevCount => prevCount + 1);
       Mak.sync();
     }, 1000);
   }, []);
-
   return (
     <React.Fragment>
       <div >{
@@ -25,7 +22,12 @@ function Temp (props){
 function representLine(data){
   return <div style={{overflow:"hidden"}} >
     <span  class="lineLabel">{data("line.name")}</span>
-    <div class="line"  onDragOver={e=> e.preventDefault()} onDrop={e=>{DnD(data("line"), e.dataTransfer.getData("text"))}}>
+    <div class="line"  
+        onDragOver={(e)=> {e.preventDefault() }} 
+        onDrop={(e)=>{
+          e.preventDefault();
+          DnD( data("line"), e.dataTransfer.getData("task") , e.nativeEvent.offsetX-e.dataTransfer.getData("dx") )
+        }}>
         {from("Task t").where("t.line=line").map(Task)}
     </div>
   </div>
@@ -34,31 +36,42 @@ function representLine(data){
 function Park(props) {
   return <div style={{overflow:"hidden"}}>
       <span class="lineLabel">Park</span>
-      <div class="line" onDragOver={e=> e.preventDefault()} onDrop={e=>{DnDPark( e.dataTransfer.getData("text"))}}>
-          {from("Task t").where("t.line=nil").map(TaskPark)} 
+      <div class="line" onDragOver={e=> e.preventDefault()} 
+                        onDrop={e=>{DnDPark( e.dataTransfer.getData("task"))}}>
+        {from("Task t").where("t.line=nil").map(TaskPark)} 
       </div>
     </div>
   
 }
 
 function Task(data){
-  const startDateMillis = data("t.startDate");
+  const startDateMillis = data("t.startDate");//
   const firstDate = new Date("01/01/2021 00:00:00");
-  const firstDateMillis =  firstDate.getTime(); 
+  const firstDateMillis =  firstDate.getTime(); //
   const differenceMillis = startDateMillis - firstDateMillis;
   const differenceDays = Math.round((differenceMillis)/(1000*60*60*24));
 
   const width = data("t.days") + "px";
   const left = differenceDays.toString() + "px";
    
-  return <div class="task" draggable="true" onDragStart={e=>{e.dataTransfer.setData("text", data("t").toString())}}  style={{left: left, width: width ,display:"inline"}}>
+  return <div class="task" draggable="true" 
+    onDragStart={(e)=>{ 
+      e.dataTransfer.setData("task", data("t").toString());
+      e.dataTransfer.setData("dx", e.nativeEvent.offsetX);
+    }}
+    style={{left: left, width: width ,display:"inline"}}>
     {data("t.customer")} 
   </div>
 }
 
 function TaskPark(data){
   let parkWidth = data("t.days") + "px";
-  return <div class="task" draggable="true" onDragStart={e=>{e.dataTransfer.setData("text", data("t").toString())}} style={{  width: parkWidth , position:"relative" }}> 
+  return <div class="task" draggable="true" 
+  onDragStart={(e)=>{ 
+    e.dataTransfer.setData("task", data("t").toString());
+    e.dataTransfer.setData("dx", e.nativeEvent.offsetX);
+  }}
+   style={{ width: parkWidth , position:"relative" }}> 
     {data("t.customer")} 
   </div>
 }
@@ -118,19 +131,25 @@ function sync(e,d,s,p) {
   const response = fetch("http://standup.csc.kth.se:8080/mak-backend/MakumbaQueryServlet", {
     method: 'POST',
     body: "object="+d+"&type=Task&path="+s+"&value="+e+"&exprType=java.lang."+p
-  });
+  }).then(()=>{Mak.sync()});
   return response; 
 }
 
-function DnD(line,t){
+function DnD(line,t,x){
   const response = fetch("http://standup.csc.kth.se:8080/mak-backend/MakumbaQueryServlet", {
     method: "POST",
     body: 
-    "updateFrom=Task%20t%2C%20ProductionLine%20line&updateSet=t.line%3Dline%2C%20t.startDate%3DdateAdd('2021-01-01'%2C%20%3Ax%2C%20'day')&updateWhere=t%3D%3At%20AND%20line%3D%3Aline&param=%7B%22t%22%3A%22"+t+"%22%2C%22line%22%3A%22"+line+"%22%2C%22x%22%3A284%7D"
-  });
+      new URLSearchParams({
+        updateFrom:"Task t, ProductionLine line",
+        updateSet: "t.line=line, t.startDate=dateAdd('2021-01-01', :x, 'day')",
+        updateWhere:'t=:t AND line=:line',
+        param:JSON.stringify({t,line,x})
+      })
+  }).then(()=>{Mak.sync()});
   return response;
 }
 
+/* for moving tasks TO the park line */
 function DnDPark(t){
   const response = fetch("http://standup.csc.kth.se:8080/mak-backend/MakumbaQueryServlet", {
     method: "POST",
